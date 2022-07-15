92bc06e624a904fe8b916cd627f52167ea9ebc67
Damian <damian.staniszewski@biziel.pl>
2022-07-15 14:27:03 +0200

    Add lombok project

--- src/main/java/com/stan/appone/configuration/AppInfo.java
+++ src/main/java/com/stan/appone/configuration/AppInfo.java
@@ -1,5 +1,8 @@
 package com.stan.appone.configuration;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -8,55 +11,30 @@ import org.springframework.context.event.EventListener;
 
 import java.util.List;
 
-
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "my.app.info")
 public class AppInfo {
 
     private String author;
+
     private int created;
+
     private String describe;
+
     private String[] authors;
+
     // Spring Expression Language (SpEL) "#{1,2,3,4}" - list of Strings
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     @Value("#{'${my.app.info.authors}'.split(',')}")
     private List<String> authorsList;
-
-    public String[] getAuthors() {
-        return authors;
-    }
-
-    public void setAuthors(String[] authors) {
-        this.authors = authors;
-    }
-
-    public String getDescribe() {
-        return describe;
-    }
-
-    public void setDescribe(String describe) {
-        this.describe = describe;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getCreated() {
-        return created;
-    }
-
-    public void setCreated(int created) {
-        this.created = created;
-    }
 
     @EventListener(ApplicationReadyEvent.class)
     public void listener(){
         System.out.println("Listener AppInfo: Author: "+ author+ ", Created: "+created);
-        System.out.println("Listener:"+authors.length+" , "+authorsList.size());
+        System.out.println("Listener: "+authors.length+" , "+authorsList.size());
     }
 
 }

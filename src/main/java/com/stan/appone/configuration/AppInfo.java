b91d3c33a127ae66b2d0f265465ca966a58d199c
Damian <damian.staniszewski@biziel.pl>
2022-07-15 13:34:22 +0200

    Modification of file management : add List

--- src/main/java/com/stan/appone/configuration/AppInfo.java
+++ src/main/java/com/stan/appone/configuration/AppInfo.java
@@ -1,9 +1,13 @@
 package com.stan.appone.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+
+import java.util.List;
+
 
 @Configuration
 @ConfigurationProperties(prefix = "my.app.info")
@@ -12,6 +16,18 @@ public class AppInfo {
     private String author;
     private int created;
     private String describe;
+    private String[] authors;
+    // Spring Expression Language (SpEL) "#{1,2,3,4}" - list of Strings
+    @Value("#{'${my.app.info.authors}'.split(',')}")
+    private List<String> authorsList;
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
 
     public String getDescribe() {
         return describe;
@@ -40,6 +56,7 @@ public class AppInfo {
     @EventListener(ApplicationReadyEvent.class)
     public void listener(){
         System.out.println("Listener AppInfo: Author: "+ author+ ", Created: "+created);
+        System.out.println("Listener:"+authors.length+" , "+authorsList.size());
     }
 
 }
